9ae86229cfe51dcc069ea9a9dd25a6f5cd54b659
Jeffery Adorkor <adorkor.jeffery@gmail.com>
2022-01-16 03:11:21 +0100

    Add currency symbols and redirect to /items page on clicking + button

--- app.js
+++ app.js
@@ -17,40 +17,45 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "/views"));
 
 //Global variables
-let currency = "";
+let currency_symbol = [];
 let totalPrice = 0;
 let amountToPay = [];
 
 // Render home page
 app.get("/", function (req, res) {
-    res.render("index");
+  res.render("index");
 });
 
-
 // POST method for the add-more button
+app.post("/", function (req, res) {
+  let currency = req.body.currency;
+  let price = req.body.price;
+  let quantity = req.body.quantity;
+  let newItemPrice = price * quantity;
 
-app.post('/', function (req, res) {
+  amountToPay.push(newItemPrice);
 
-    currency = req.body.currency;
-    let price = req.body.price;
-    let quantity = req.body.quantity;
-    let newItemPrice = price * quantity;
+  currency_symbol.push(currency);
+  totalPrice += newItemPrice;
 
-    amountToPay.push(newItemPrice);
+  res.redirect("/items");
+});
 
-    totalPrice += newItemPrice;
-
-    // res.render("index", { currency, amountToPay });
+//Render items page
+app.get("/items", function (req, res) {
+  let symbol = currency_symbol[0];
+  res.render("items", { symbol: symbol });
 });
 
 //Calculate total cost
-
 app.post("/result", function (req, res) {
-    let cost = totalPrice;
-    res.render("result", { currency: currency, cost: cost });
+  let symbol = currency_symbol[0];
+  let cost = totalPrice;
+
+  res.render("result", { symbol: symbol, cost: cost });
 });
 
 // Port
 app.listen(port, function () {
-    console.log("Server listening at port " + port);
+  console.log("Server listening at port " + port);
 });
